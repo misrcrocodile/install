b79e0e906953caa9a7977e680b9398cc0285adf0
misscrocodile <misr.crocodile@gmail.com>
2019-09-10 23:13:39 +0900

    add nodejs for armv7 Pi4

--- nodejs.sh
+++ nodejs.sh
@@ -8,16 +8,21 @@ mkdir -p installNodejsTemp
 cd installNodejsTemp
 
 PROCESS_TYPE=$(uname -m)
+
 NODEJS_LINK='https://nodejs.org/dist/v10.15.0/node-v10.15.0-linux-x64.tar.xz'
 
 if [[ $PROCESS_TYPE = armv6* ]]; then
   NODEJS_LINK='https://nodejs.org/dist/v10.15.0/node-v10.15.0-linux-armv6l.tar.xz'
 fi
 
-wget $NODEJS_LINK -O node-v10.15.0.tar.xz
-mkdir -p node-v10.15.0
-tar xf node-v10.15.0.tar.xz -C node-v10.15.0 --strip-components=1
-cd node-v10.15.0
+if [[ $PROCESS_TYPE = armv7* ]]; then
+  NODEJS_LINK='https://nodejs.org/dist/v10.16.3/node-v10.16.3-linux-armv7l.tar.xz'
+fi
+
+wget $NODEJS_LINK -O node-v10.tar.xz
+mkdir -p node-v10
+tar xf node-v10.tar.xz -C node-v10 --strip-components=1
+cd node-v10
 sudo cp -R * /usr/local/
 
 # Install PM2
